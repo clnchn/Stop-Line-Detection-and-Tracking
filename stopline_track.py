b92899bba71c481c6bf85022d706da5671b5c00b
hl5chen <hsuan_ling@hotmail.com>
2019-10-08 15:42:53 -0400

    wip

--- stopline_track.py
+++ stopline_track.py
@@ -21,16 +21,16 @@ import math
 #return croppededges
 
 
-frame = cv2.imread('/home/celene/python_environment/stop-junction2.jpg')
+frame = cv2.imread('/home/celene/python_environment/stop-junction3.jpg')
 hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
 lower_blue = np.array([0, 0, 10])
-upper_blue = np.array([200, 200, 150])
+upper_blue = np.array([200, 200, 100])
 mask = cv2.inRange(hsv, lower_blue, upper_blue)
 edges = cv2.Canny(mask, 200, 400)
 
 
-lines = cv2.HoughLinesP(edges,1,np.pi/180,100,minLineLength=100,maxLineGap=10)
+lines = cv2.HoughLinesP(edges,1,np.pi/180,80,minLineLength=100,maxLineGap=10)
 for line in lines:
     x1,y1,x2,y2 = line[0]
     cv2.line(frame,(x1,y1),(x2,y2),(0,255,0),2)
@@ -38,9 +38,8 @@ for line in lines:
 # Eliminate roadlines that are not inside the drivable surface based on segmentation. Separate into stop lines and lane lines.
 
 cv2.imshow('image', edges)
-cv2.waitKey(2000)
-cv2.imshow('image', frame)
-cv2.waitKey(5000)
+#cv2.imshow('image', frame)
+cv2.waitKey()
 
 
 print("Hello World")
