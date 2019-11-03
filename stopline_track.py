1e1e0b5f22a51578629c9f941b1a5519bc3de508
hl5chen <hsuan_ling@hotmail.com>
2019-11-03 12:17:44 -0500

    added new test input

--- stopline_track.py
+++ stopline_track.py
@@ -25,8 +25,8 @@ def selectROI(edges):
 
 	#We shall only focus on the bottom half of the screen which are 	the lane and stop lane 
 	polygon = np.array([[
-        (0, height * 9 / 16), 
-        (width, height * 9 / 16),
+        (0, height * 3 / 16),  #(0, height * 9 / 16), 
+        (width, height * 6 / 16), #(width, height * 9 / 16),
         (width, height),
         (0, height),
     	]], np.int32)
@@ -37,61 +37,19 @@ def selectROI(edges):
 	return cropped_edges
 
 
-def average_slope_intercept(frame, line_segments):
-    """
-    This function combines line segments into one or two lane lines
-    If all line slopes are < 0: then we only have detected left lane
-    If all line slopes are > 0: then we only have detected right lane
-    """
-    lane_lines = []
-    if line_segments is None:
-        logging.info('No line_segment segments detected')
-        return lane_lines
-
-    height, width, _ = frame.shape
-    left_fit = []
-    right_fit = []
-
-
-#These might need to be height for horizotal lines 
-    boundary = 1/3
-    left_region_boundary = width * (1 - boundary)  # left lane line segment should be on left 2/3 of the screen
-    right_region_boundary = width * boundary # right lane line segment should be on left 2/3 of the screen
-
-
-    for line_segment in line_segments:
-        for x1, y1, x2, y2 in line_segment:
-            if x1 == x2:
-                logging.info('skipping vertical line segment (slope=inf): %s' % line_segment)
-                continue
-            fit = np.polyfit((x1, x2), (y1, y2), 1)
-            slope = fit[0]
-            intercept = fit[1]
-            if slope < 0:
-                if x1 < left_region_boundary and x2 < left_region_boundary:
-                    left_fit.append((slope, intercept))
-            else:
-                if x1 > right_region_boundary and x2 > right_region_boundary:
-                    right_fit.append((slope, intercept))
-
-    left_fit_average = np.average(left_fit, axis=0)
-    if len(left_fit) > 0:
-        lane_lines.append(make_points(frame, left_fit_average))
-
-    right_fit_average = np.average(right_fit, axis=0)
-    if len(right_fit) > 0:
-        lane_lines.append(make_points(frame, right_fit_average))
-
-    logging.debug('lane lines: %s' % lane_lines)  # [[[316, 720, 484, 432]], [[1009, 720, 718, 432]]]
-
-    return lane_lines
 
 #======= Main =======
 
 # Loads an image
 #cv.samples.findFile(filename)<----Change Path otherwise stuff wont work
 
-frame = cv2.imread('/home/celene/git_workspace/Stop-Line-Detection-and-Tracking/stop-junction2.jpg')
+frame = cv2.imread('/home/celene/git_workspace/Stop-Line-Detection-and-Tracking/Test1.jpg')
+scale_percent = 25 # percent of original size
+width = int(frame.shape[1] * scale_percent / 100)
+height = int(frame.shape[0] * scale_percent / 100)
+dim = (width, height)
+
+frame = cv2.resize(frame, dim, interpolation = cv2.INTER_AREA)
 
 #Check if image is loaded fine
 #if frame is None: 
@@ -132,12 +90,13 @@ except:
 
 # Dashed Lines 
 
-cv2.imwrite('/home/celene/git_workspace/Stop-Line-Detection-and-Tracking/output.jpg',frame)
+cv2.imwrite('/home/celene/git_workspace/Stop-Line-Detection-and-Tracking/result.jpg',frame)
 
-
-cv2.imshow('image', focus_edges)
-cv2.imshow('image2', frame)
+cv2.imshow('hsv', hsv)
+cv2.imshow('edges', focus_edges)
+cv2.imshow('output', frame)
 cv2.waitKey(50000)
+cv2.imwrite('/home/celene/git_workspace/Stop-Line-Detection-and-Tracking/output.jpg',frame)
 
 
 print("Program Ends")
